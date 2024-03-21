e327a17cec436ab96b439e9f59989d35980bc990
綾雪 <2239301685@qq.com>
2024-03-21 22:45:45 +0800

    商家权限划分

--- backend/src/main/java/com/Shopping/service/AssessService.java
+++
@@ -1,8 +0,0 @@
-package com.Shopping.service;
-
-import com.baomidou.mybatisplus.extension.service.IService;
-
-
-public interface AssessService extends IService<Assess> {
-
-}
--- backend/src/main/java/com/Shopping/service/ConfirmService.java
+++
@@ -1,8 +0,0 @@
-package com.Shopping.service;
-
-import com.baomidou.mybatisplus.extension.service.IService;
-
-
-public interface ConfirmService extends IService<Confirm> {
-
-}
