2b2bfa8a1a1296cfb8c9dbdd00954827a916bb09
綾雪 <2239301685@qq.com>
2024-03-30 10:48:45 +0800

    删除secretKey

--- backend/src/main/java/com/Shopping/controller/HistoryController.java
+++ backend/src/main/java/com/Shopping/controller/HistoryController.java
@@ -15,7 +15,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/img")
-public class ImgController {
+public class HistoryController {
 
     @Resource
     private HistoryMapper historyMapper;
