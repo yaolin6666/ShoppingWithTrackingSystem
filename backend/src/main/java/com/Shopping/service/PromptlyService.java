2c07b66360a40ddaac94d036588c5db81f5ece84
綾雪 <2239301685@qq.com>
2024-03-30 10:57:29 +0800

    清理未使用domain

--- backend/src/main/java/com/Shopping/service/PromptlyService.java
+++
@@ -1,9 +0,0 @@
-package com.Shopping.service;
-
-import com.baomidou.mybatisplus.extension.service.IService;
-import com.Shopping.domain.Promptly;
-
-
-public interface PromptlyService extends IService<Promptly> {
-
-}
